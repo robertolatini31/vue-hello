c0ceae3ecd7d064fd4e69b0c30f70121ccc6ca60
robertolatini31 <robertolatini31@gmail.com>
2022-04-20 13:41:35 +0200

    added message

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,7 +2,10 @@ let app = new Vue (
     {
         el: '#app',
         data: {
-            
+            message: 'Hello VueJS'
         }
     }
 )
+
+
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
